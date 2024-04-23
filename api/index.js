a213404a0fcb253184beaf6264933219bc4efc20
Anmol Gupta <anmolgupta202014@gmail.com>
2024-04-23 17:54:27 +0530

    fix: vercel deploy

--- api/index.js
+++ api/index.js
@@ -18,10 +18,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
 
-const PORT = 8000;
+const PORT = 3000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+// app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+
+export default app
