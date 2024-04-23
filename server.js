b33c48ab24f5b2c91607e85e691d1d5926216cd8
Anmolguptaaa <anmolgupta202014@gmail.com>
2024-04-23 16:52:50 +0530

    update

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-    origin:["http://localhost:3000/","https://blog-app.onrender.com"],
-}));
+app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
